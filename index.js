3ae3713d5644a72baeabba83b885a02221d51a13
dorsa-manouchehri <d.manouchehri.a@gmail.com>
2023-06-18 18:35:31 +0330

    added the the icon fetched from api

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ let minutes = now.getMinutes();
 let hours = now.getHours();
 let datee = now.getDate();
 let year = now.getFullYear();
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 let day = days[now.getDay()];
 let months = [
   "Jan",
@@ -18,26 +26,33 @@ let months = [
   "Sep",
   "Oct",
   "Nov",
-  "Dec"
+  "Dec",
 ];
 let month = months[now.getMonth()];
 let span = document.querySelector("#date");
 span.innerHTML = ` ${day}, ${month}, ${year}, ${hours}:${minutes}`;
-function displayTemperature(response){
-    console.log(response.data)
-    let temperatureElement= document.querySelector("#temperature");
-    temperatureElement.innerHTML=Math.round(response.data.main.temp);
-    let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=response.data.name;
-    let descriptioElement=document.querySelector("#description");
-    descriptioElement.innerHTML=response.data.weather[0].description;
-    let humidityElement=document.querySelector("#humidity");
-    humidityElement.innerHTML=response.data.main.humidity;
-    let windElement= document.querySelector("#wind");
-    windElement.innerHTML=response.data.wind.speed;
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptioElement = document.querySelector("#description");
+  descriptioElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey="5f472b7acba333cd8a035ea85a0d4d4c";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=Tehran&appid=${apiKey}&units=metric`  ;  
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let city = "Tehran";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
