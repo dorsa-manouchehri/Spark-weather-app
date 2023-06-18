9e347712c8501bc64a69385abe505df191ff69e7
dorsa-manouchehri <134857640+dorsa-manouchehri@users.noreply.github.com>
2023-06-18 16:09:45 +0330

    added temp, wind, and humidity

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ function displayTemperature(response){
     console.log(response.data.main.temp)
     let temperatureElement= document.querySelector("#temperature");
     temperatureElement.innerHTML=Math.round(response.data.main.temp);
+    let cityElement=document.querySelector("#city");
+    cityElement.innerHTML=response.data.name;
+    let descriptioElement=document.querySelector("#description");
+    descriptioElement.innerHTML=response.data.weather[0].description;
+    let humidityElement=document.querySelector("#humidity");
+    humidityElement.innerHTML=response.data.main.humidity;
+    let windElement= document.querySelector("#wind");
+    windElement.innerHTML=response.data.wind.speed;
 }
 
 let apiKey="5f472b7acba333cd8a035ea85a0d4d4c";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=Tehran&appid=${apiKey}&units=metric`  ;  
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=Qom&appid=${apiKey}&units=metric`  ;  
 
 axios.get(apiUrl).then(displayTemperature);
