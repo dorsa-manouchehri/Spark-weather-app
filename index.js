15c811f9f29e3d23dcbd6068588f4e1098f594c0
dorsa-manouchehri <134857640+dorsa-manouchehri@users.noreply.github.com>
2023-06-19 10:49:29 +0330

    made the converter work

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ let minutes = now.getMinutes();
 let hours = now.getHours();
 let datee = now.getDate();
 let year = now.getFullYear();
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 let day = days[now.getDay()];
 let months = [
   "Jan",
@@ -18,26 +26,72 @@ let months = [
   "Sep",
   "Oct",
   "Nov",
-  "Dec"
+  "Dec",
 ];
 let month = months[now.getMonth()];
 let span = document.querySelector("#date");
 span.innerHTML = ` ${day}, ${month}, ${year}, ${hours}:${minutes}`;
-function displayTemperature(response){
-    console.log(response.data)
-    let temperatureElement= document.querySelector("#temperature");
-    temperatureElement.innerHTML=Math.round(response.data.main.temp);
-    let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=response.data.name;
-    let descriptioElement=document.querySelector("#description");
-    descriptioElement.innerHTML=response.data.weather[0].description;
-    let humidityElement=document.querySelector("#humidity");
-    humidityElement.innerHTML=response.data.main.humidity;
-    let windElement= document.querySelector("#wind");
-    windElement.innerHTML=response.data.wind.speed;
+function displayTemperature(response) {
+  console.log(response.data);
+  celsiusTemp = response.data.main.temp;
+  let temperatureElement = document.querySelector("#temperature");
+
+  let cityElement = document.querySelector("#city");
+
+  let descriptioElement = document.querySelector("#description");
+
+  let humidityElement = document.querySelector("#humidity");
+
+  let windElement = document.querySelector("#wind");
+
+  let iconElement = document.querySelector("#icon");
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
+  cityElement.innerHTML = response.data.name;
+  descriptioElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+function submit(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#cityInput");
+  search(cityElement.value);
+}
+function displayFahrenheitTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+}
+function displayCelsiustemp(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
 }
 
-let apiKey="5f472b7acba333cd8a035ea85a0d4d4c";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=Tehran&appid=${apiKey}&units=metric`  ;  
+let celsiusTemp = null;
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", submit);
 
-axios.get(apiUrl).then(displayTemperature);
+let fahrenheitLink = document.querySelector("#fahrenheitValue");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsiusValue");
+celsiusLink.addEventListener("click", displayCelsiustemp);
+search("Rome");
